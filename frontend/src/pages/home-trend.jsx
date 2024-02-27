eb8a1bd0a544000ff6e5faede27cf5ff90eb7c4d
Sparsh Manohar <117460823+Sparsh7219@users.noreply.github.com>
2024-02-27 18:38:50 +0530

    added pages and components

---
+++ frontend/src/pages/home-trend.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+function Home() {
+    return (
+        <>
+          <Navbar/>
+          <h1>Home</h1>
+          <Footer/>
+
+        </>
+    );
+}
+
+export default Home;
