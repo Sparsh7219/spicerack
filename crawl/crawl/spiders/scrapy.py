0615d2438f808c35aa965cc39d80b78359adacdc
GTX18 <abhishekhaldankar18@gmail.com>
2024-02-29 23:08:52 +0530

    Lessgoo it works

--- crawl/crawl/spiders/scrapy.py
+++ crawl/crawl/spiders/scrapy.py
@@ -5,7 +5,9 @@ class CrawlingSpider(CrawlSpider):
     name="food"
     allowed_domains=["food.com"]
     start_urls=["https://www.food.com/",
-                #"https://www.food.com/ideas/top-recipes-for-kids-6975?ref=nav#c-811546"
+                "https://www.food.com/ideas/top-recipes-for-kids-6975?ref=nav#c-811546",
+                "https://www.food.com/ideas/summer-cocktails-drinks-6268?ref=nav#c-517255"
+                "https://www.food.com/ideas/best-instant-pot-recipes-6928?ref=nav#c-789324"
                 ]
 
     rules=(
@@ -14,30 +16,17 @@ class CrawlingSpider(CrawlSpider):
     
 
     def parse_item(self,response):
-        ingredients = []
         # Extracting ingredients with quantity and link
-        """"ingredient_elements = response.css("ul.ingredient-list.svelte-1dqq0pw li")
-        for ingredient_element in ingredient_elements:
-            quantity = ingredient_element.css("span.ingredient-quantity.svelte-1dqq0pw::text").get()
-            link = ingredient_element.css("span.ingredient-text.svelte-1dqq0pw a::attr(href)").get()
-            ingredient_text = ingredient_element.css("span.ingredient-text.svelte-1dqq0pw::text").get()
-            # Combining quantity and ingredient text
-            if quantity:
-                ingredient = f"{quantity.strip()} {ingredient_text.strip()}"
-            else:
-                ingredient = ingredient_text.strip()
-            # Adding the link if available
-            if link:
-                ingredient += f" ({response.urljoin(link)})"
-            ingredients.append(ingredient)"""
+       
         ingredients = []
         ingredient_items = response.css("ul.ingredient-list.svelte-1dqq0pw li")
 
         for item in ingredient_items:
             quantity = item.css("span.ingredient-quantity.svelte-1dqq0pw::text").get()
-            ingredient_text = item.css("span.ingredient-text::text").getall()
+            ingredient_text = ' '.join(item.css("span.ingredient-text::text").getall())
             ingredient_name = item.css("span.ingredient-text.svelte-1dqq0pw a::text").get()
-            ingredients.append(f"{quantity} {ingredient_text} {ingredient_name}")
+            ingredient = f"{quantity} {ingredient_text} {ingredient_name}".strip().replace("\n", '').replace('  ', '')
+            ingredients.append(ingredient)
 
 
         # Cleaning up the ingredients list
