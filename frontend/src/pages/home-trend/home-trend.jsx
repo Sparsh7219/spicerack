1ea99b44aaf03514d7bba4f21cb978e461fdb60c
Sparsh Manohar <117460823+Sparsh7219@users.noreply.github.com>
2024-03-08 15:25:15 +0530

    frent

--- frontend/src/pages/home-trend/home-trend.jsx
+++ frontend/src/pages/home-trend/home-trend.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "../../components/navbar/navbar";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/footer/footer";
 
 function Home() {
     return (
         <>
           <Navbar/>
-            
+          
           <Footer/>
 
         </>
