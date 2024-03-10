5cd83211050ffe4b49a358993ba23a023ca8d2eb
Fayeez17 <136831174+Fayeez17@users.noreply.github.com>
2024-03-10 23:21:04 +0530

    changes in ingredients

--- tool/change.py
+++ tool/change.py
@@ -3,19 +3,19 @@ import json
 def update_ingredients(json_data):
     for item in json_data:
         if 'ingredients' in item:
-            item['ingredients'] = [ingredient.replace('ground turmeric', 'turmeric') for ingredient in item['ingredients']]
+            item['ingredients'] = [ingredient.replace('salted butter', 'butter') for ingredient in item['ingredients']]
     return json_data
 
 # Read the JSON file
 file_path = "recipes.json"  # Replace with your actual file path
-with open(file_path, 'r') as file:
-    data = json.load(file)
+with open(r'tool\recipes.json', 'r',encoding='utf-8') as f:
+    data = json.load(f)
 
 # Update the ingredients
 updated_data = update_ingredients(data)
 
 # Write the updated data back to the JSON file
-with open(file_path, 'w') as file:
-    json.dump(updated_data, file, indent=2)
+with open(r'tool\recipes.json', 'w',encoding='utf-8') as f:
+    json.dump(updated_data, f, indent=2)
 
 print("Ingredients updated successfully.")
