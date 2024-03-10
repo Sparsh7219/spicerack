cbe1fd2511c7088e0a581eac82ed75669eefe8c0
GTX18 <abhishekhaldankar18@gmail.com>
2024-03-10 20:49:07 +0530

    5000th

--- tool/change.py
+++ tool/change.py
@@ -3,7 +3,7 @@ import json
 def update_ingredients(json_data):
     for item in json_data:
         if 'ingredients' in item:
-            item['ingredients'] = [ingredient.replace('cheddar cheese blend', 'cheddar cheese') for ingredient in item['ingredients']]
+            item['ingredients'] = [ingredient.replace('active yeast', 'yeast') for ingredient in item['ingredients']]
     return json_data
 
 # Read the JSON file
