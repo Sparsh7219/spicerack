091e49474fb6107b2265dc4c51365a73e1594998
GTX18 <abhishekhaldankar18@gmail.com>
2024-03-13 20:09:32 +0530

    10220th

--- tool/change.py
+++ tool/change.py
@@ -3,7 +3,7 @@ import json
 def update_ingredients(json_data):
     for item in json_data:
         if 'ingredients' in item:
-            item['ingredients'] = [ingredient.replace('large shrimp', 'shrimp') for ingredient in item['ingredients']]
+            item['ingredients'] = [ingredient.replace('refined flour', 'flour') for ingredient in item['ingredients']]
     return json_data
 
 # Read the JSON file
