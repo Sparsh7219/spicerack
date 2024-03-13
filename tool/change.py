402a7a516ee76829442e168d6785f7249b58b563
GTX18 <abhishekhaldankar18@gmail.com>
2024-03-13 21:48:19 +0530

    changes

--- tool/change.py
+++ tool/change.py
@@ -3,7 +3,7 @@ import json
 def update_ingredients(json_data):
     for item in json_data:
         if 'ingredients' in item:
-            item['ingredients'] = [ingredient.replace('lemon juice', 'lemon') for ingredient in item['ingredients']]
+            item['ingredients'] = [ingredient.replace('za&#39;atar spice mix', 'spice mix') for ingredient in item['ingredients']]
     return json_data
 
 # Read the JSON file
