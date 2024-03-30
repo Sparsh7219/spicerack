1b69f2c137d5b552d0c1b3412637833161abda4a
Fayeez17 <136831174+Fayeez17@users.noreply.github.com>
2024-03-30 21:10:22 +0530

    added cors

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, session, redirect, url_for
 from routes.search_routes import bp as search_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,resources={r"/*":{"origins":"*"}})
 
 app.register_blueprint(search_bp)
 
